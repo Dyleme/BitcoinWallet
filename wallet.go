0d6d1e8a6d00940b47eabd25630a6bcd748a24ca
Dyleme <aleksei.dyavgo@gmail.com>
2021-08-31 16:31:58 +0300

    Add periods to comments

--- wallet.go
+++ wallet.go
@@ -30,7 +30,7 @@ type Wallet struct {
 // Withdraw method provides method to withdraw BitCoins from the wallet
 // It returns updated amount of BitCoins in the wallet and error, if it occurs.
 // The argument should be positive, if its not method will return NonPositiveArgumentError
-// If after withdraw balance is negative, method will return NotHaveEnoughMoneyError
+// If after withdraw balance is negative, method will return NotHaveEnoughMoneyError.
 func (w *Wallet) Withdraw(coins BitCoin) (BitCoin, error) {
 	if coins <= 0 {
 		return w.balance, fmt.Errorf("withdraw: %w, amount = %v", ErrNotPositiveArgumentError, coins)
@@ -50,7 +50,7 @@ func (w *Wallet) Withdraw(coins BitCoin) (BitCoin, error) {
 
 // Deposit method provides method to deposit BitCoins from the wallet
 // It returns updated amount of BitCoins in the wallet and error, if it occurs.
-// The argument should be positive, if its not method will return NonPositiveArgumentError
+// The argument should be positive, if its not method will return NonPositiveArgumentError.
 func (w *Wallet) Deposit(coins BitCoin) (BitCoin, error) {
 	if coins <= 0 {
 		return w.balance, fmt.Errorf("depost: %w, funds amount = %v", ErrNotPositiveArgumentError, coins)
@@ -64,7 +64,7 @@ func (w *Wallet) Deposit(coins BitCoin) (BitCoin, error) {
 	return w.balance, nil
 }
 
-// Balance method provides method to get Balance of the wallet
+// Balance method provides method to get Balance of the wallet.
 func (w *Wallet) Balance() BitCoin {
 	w.mx.RLock()
 	defer w.mx.RUnlock()
@@ -72,7 +72,7 @@ func (w *Wallet) Balance() BitCoin {
 	return w.balance
 }
 
-// NewWallet make instance of wallet
+// NewWallet make instance of wallet.
 func NewWallet(coins BitCoin) Wallet {
 	return Wallet{balance: coins}
 }
